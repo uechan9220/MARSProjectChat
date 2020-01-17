59c74e3be7793c35bea9b244e4937b71d2025b37
MokeeeMokeee <moke.0922.moke@gmail.com>
2020-01-17 14:05:14 +0900

    main view fix

--- components/loginView.vue
+++ components/loginView.vue
@@ -1,17 +1,21 @@
 <template>
   <div class="loginBox">
-    <video
-      id="bg-video"
-      src="~/assets/videos/example.mp4"
-      poster="~/assets/images/example.png"
-      type="video/mp4"
-      autoplay
-      loop
-    ></video>
+    <video id="bg-video" src="~/assets/videos/example.mp4" type="video/mp4" autoplay loop></video>
     <div class="loginContent">
       <div class="loginContentText">
         <p>MARS</p>
         <p>PROJECT</p>
+      </div>
+      <div class="loginButtonBox">
+        <div>
+          <p class="loginText">Login</p>
+          <div @click="googleLogin" class="googleLoginButtonBox">
+            <div class="googleLoginButtonImageBox">
+              <img src="~/assets/images/google-icon.svg" class="googleLoginButtonImage" />
+            </div>
+            <p class="googleLoginButtonText">Login with Google</p>
+          </div>
+        </div>
       </div>
     </div>
   </div>
@@ -57,7 +61,7 @@ export default Vue.extend({
         margin: 0;
       }
       font-size: 10rem;
-      margin: -15rem 0 0 1rem;
+      margin: -5rem 0 0 1rem;
       font-weight: bold;
       position: relative;
       color: #ff7c00;
@@ -70,6 +74,57 @@ export default Vue.extend({
       );
       -webkit-background-clip: text;
       -webkit-text-fill-color: transparent;
+    }
+  }
+  &Button {
+    &Box {
+      display: flex;
+      align-items: flex-end;
+      margin: 5rem 1rem 0 0;
+      flex-direction: column;
+    }
+  }
+  &Text {
+    color: #fff;
+    font-family: Roboto, arial, sans-serif;
+    font-weight: 500;
+    letter-spacing: 0.21px;
+    font-size: 3rem;
+    text-align:center;
+    margin:1rem 0;
+  }
+}
+
+.google {
+  &Login {
+    &Button {
+      &Box {
+        background-color: #fff;
+        margin: 1px;
+        display: inline-flex;
+        align-items: center;
+        background-color: #4285f4;
+        cursor: pointer;
+      }
+      &Image {
+        min-width: 2rem;
+        min-height: 2rem;
+        max-width: 2rem;
+        max-height: 2rem;
+        margin: 15px;
+        &Box {
+          background-color: #fff;
+          margin: 1px;
+        }
+      }
+      &Text {
+        font-family: Roboto, arial, sans-serif;
+        font-weight: 500;
+        letter-spacing: 0.21px;
+        font-size: 16px;
+        color: #fff;
+        padding: 16px 32px;
+      }
     }
   }
 }
