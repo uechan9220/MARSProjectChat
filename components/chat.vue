108e14865b69b3e01d7e86559c8eeb74a205dd9c
MokeeeMokeee <moke.0922.moke@gmail.com>
2020-01-17 11:14:24 +0900

    login add

---
+++ components/chat.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <div>
+      <div class="accountBox">
+        <img class="accountBoxImage" :src="items.photoURL" />
+        <p>{{ items.displayName }}</p>
+      </div>
+    </div>
+    <button @click="logOut">ログアウト</button>
+    <button @click="hoge">aaaaaaaaaa</button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["items"],
+  methods: {
+    logOut() {
+      firebase.auth().signOut();
+    },
+    hoge() {
+      console.log(this.items);
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.account {
+  &Box {
+    display: flex;
+    align-items: center;
+    &Image {
+      max-height: 4rem;
+      max-width: 4rem;
+      border-radius: 50%;
+    }
+  }
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,72 +1,57 @@
 <template>
-  <div class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        MARSProjectChat
-      </h1>
-      <h2 class="subtitle">
-        My praiseworthy Nuxt.js project
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
+  <section class="container">
+    <div v-if="isWaiting">
+      <p>読み込み中</p>
+    </div>
+    <div v-else>
+      <div v-if="!isLogin">
+        <button @click="googleLogin">Googleでログイン</button>
+      </div>
+      <div v-else>
+        <Chat :items='user'/>
       </div>
     </div>
-  </div>
+  </section>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
+import firebase from '@/plugins/firebase'
+import Chat from '~/components/chat.vue'
 
 export default {
   components: {
-    Logo
+    Chat
+  },
+  asyncData () {
+    return {
+      isWaiting: true,
+      isLogin: false,
+      user: []
+    }
+  },
+  mounted() {
+    firebase.auth().onAuthStateChanged(user => {
+      this.isWaiting = false
+      if (user) {
+        this.isLogin = true
+        this.user = user
+      } else {
+        this.isLogin = false
+        this.user = []
+      }
+    })
+  },
+  methods: {
+    googleLogin () {
+      const provider = new firebase.auth.GoogleAuthProvider()
+      firebase.auth().signInWithRedirect(provider)
+    },
+    logOut () {
+      firebase.auth().signOut()
+    },
+    hoge(){
+      console.log(this.user)
+    }
   }
 }
 </script>
-
-<style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
-</style>
