a1f5f841979c6e287801ee691fbe8a3a6a6613ca
MokeeeMokeee <moke.0922.moke@gmail.com>
2020-01-17 14:09:49 +0900

    box-shadow add

--- components/loginView.vue
+++ components/loginView.vue
@@ -105,6 +105,7 @@ export default Vue.extend({
         align-items: center;
         background-color: #4285f4;
         cursor: pointer;
+        box-shadow: 0 2px 2px 0 rgba(0,0,0,0.14), 0 3px 1px -2px rgba(0,0,0,0.12), 0 1px 5px 0 rgba(0,0,0,0.2);
       }
       &Image {
         min-width: 2rem;
