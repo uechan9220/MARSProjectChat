3f84037df9d03efce1e98e0a39c4c93b65f80c3d
MokeeeMokeee <moke.0922.moke@gmail.com>
2020-01-27 14:44:18 +0900

    fix

--- components/chat/rooms.vue
+++ components/chat/rooms.vue
@@ -1,6 +1,50 @@
 <template>
-  <div>
+  <div class="roomsBox">
+    <h1 class="roomsTitle">Rooms</h1>
     <!-- ここにv-forでroome情報を持ってくる様にする -->
-    <h1>hoge</h1>
+    <div v-for="item in items" :key="item.id">
+      <h1 class="roomsText">{{item.name}}</h1>
+    </div>
   </div>
 </template>
+
+<script>
+import firebase from "firebase";
+const db = firebase.firestore();
+
+export default {
+  data() {
+    return {
+      items: []
+    };
+  },
+  async mounted() {
+    const res = await db.collection("rooms").get();
+    res.forEach(doc => {
+      this.items.push({ name: doc.id });
+    });
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.rooms {
+  &Text {
+    color: #fff;
+    margin: 1rem 0;
+  }
+  &Box {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    height: 90vh;
+    border-top: 2px solid #fff;
+    border-bottom: 2px solid #fff;
+  }
+  &Title{
+    color:#fff;
+    margin: 1rem 0 0 0;
+    
+  }
+}
+</style>
